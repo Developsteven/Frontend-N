a54fb14e7d5920879f8346e234413a95d8ad9d1a
Developsteven <steven.perez099@gmail.com>
2021-05-18 13:17:44 -0500

    Generando los services del token.

--- src/app/token/auth.service.ts
+++ src/app/token/auth.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { JwtDTO } from '../dto/jwt-dto';
+import { LoginUsuario } from '../dto/login-usuario';
+import { NuevoUsuario } from '../dto/nuevo-usuario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  authURL = 'http://localhost:8089/auth/';
+
+  constructor(private httpClient: HttpClient) { }
+
+  public nuevo(nuevoUsuario: NuevoUsuario): Observable<any> {
+    return this.httpClient.post<any>(this.authURL + 'nuevo', nuevoUsuario);
+  }
+
+  public login(loginUsuario: LoginUsuario): Observable<JwtDTO> {
+    return this.httpClient.post<JwtDTO>(this.authURL + 'login', loginUsuario);
+  }
 }
