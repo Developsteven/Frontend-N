0dbcf8faa10a764fa5b2f3429cbea7f4712284cd
Developsteven <steven.perez099@gmail.com>
2021-05-30 19:19:54 -0500

    Implementando authinterceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ import { LoginComponent } from './usuarios/login/login.component';
 import { AuthGuard } from './usuarios/guards/auth.guard';
 import { RoleGuard } from './usuarios/guards/role.guard';
 import { TokenInterceptor }from './usuarios/interceptors/token.interceptor';
+import { AuthInterceptor }from './usuarios/interceptors/auth.interceptor';
 
 
 
@@ -70,7 +71,8 @@ const routes: Routes =[
   ],
   providers: [AprendizService,
     { provide: LOCALE_ID, useValue: 'es' },
-    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
   ],
   bootstrap: [AppComponent]
 })
